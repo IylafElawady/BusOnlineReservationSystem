1bd4b3caf8687dfb621a078c984536e1fc501a3d
IskanderSamir <eskandr.samir@fci.helwan.edu.eg>
2018-04-21 02:52:25 -0700

    First Commet

---
+++ BusOnlineReservationSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BusOnlineReservationSystem.MvcApplication" Language="C#" %>
---
+++ BusOnlineReservationSystem/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using Ilaro.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BusOnlineReservationSystem
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AdminInitialise.RegisterRoutes(RouteTable.Routes, prefix: "Admin");
+            AdminInitialise.RegisterResourceRoutes(RouteTable.Routes);
+        }
+    }
+}
